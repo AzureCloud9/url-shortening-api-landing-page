fb2613cb55e65003eda665d5a6a41c4f85fe0f2f
MarleyM1999 <marleyalmere@gmail.com>
2023-07-19 22:38:00 +0200

    update

---
+++ src/components/FooterHero.jsx
@@ -0,0 +1,9 @@
+
+export default function FooterHero() {
+  return (
+    <div className=" w-[100vw] h-[300px] flex justify-center items-center flex-col bg-color4 " >
+        <h1 className="text-[28px] font-bold text-white mb-[24px]" >Boost your links today</h1>
+        <div className="w-[195px] h-[56px] bg-color5 flex justify-center items-center rounded-2xl"><p className="text-white text-[20px] font-bold">Get Started</p></div>
+    </div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from '../components/Hero'
 import UrlShortener from '../components/UrlShortener'
 import InfoSection from '../components/InfoSection'
+import FooterHero from '../components/FooterHero'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
@@ -9,6 +11,8 @@ export default function Home() {
         <Hero/>
         <UrlShortener/>
         <InfoSection/>
+        <FooterHero/>
+        <Footer/>
     </div>
   )
 }
