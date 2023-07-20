1c485626f959981108dc9af1a5169083ea28b339
MarleyM1999 <marleyalmere@gmail.com>
2023-07-20 02:32:44 +0200

    update

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -41,7 +41,20 @@ export default function RootLayout() {
         <Outlet/>
     </div>
 
-    
+    <div className="lg:block">
+        <div className="h-[80px] w-[100vw]  flex  bg-white  ">
+            <img className="w-[120px] h-[32px] ml-[167px] mt-[55px] mr-[45px]" src={logo} alt="pic"/>
+            <h2 className="text-[18px] font-bold text-color2 mt-[58px] mr-[29px] cursor-pointer hover:text-color6 ">Features</h2>
+            <h2 className="text-[18px] font-bold text-color2 mt-[58px] mr-[29px] cursor-pointer hover:text-color6">Pricing</h2>
+            <h2 className="text-[18px] font-bold text-color2 mt-[58px] mr-[493px] cursor-pointer hover:text-color6">Resources</h2>
+            <h1 className="text-[18px] font-bold text-color2 mt-[58px] mr-[37px]  cursor-pointer hover:text-color6  ">login</h1>
+             <div className="w-[105px] h-[40px] rounded-3xl mt-[48px] bg-color5 flex justify-center items-center hover:bg-color7">
+                <p className="text-[18px] font-bold text-white">Sign Up</p>
+             </div>
+        </div>
+        <Outlet/>
+    </div>
+
     
 </>
 
